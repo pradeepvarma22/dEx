f8b70e6ef71d174ff953a19bf48e10d57e61d1d1
pradeepvarma22 <pradeepnadimpalli22@gmail.com>
2022-10-17 15:41:29 +0530

    add: deposit
    Signed-off-by: pradeepvarma22 <pradeepnadimpalli22@gmail.com>

--- dex-contract/contracts/Exchange.sol
+++ dex-contract/contracts/Exchange.sol
@@ -1,13 +1,42 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.9;
 import "hardhat/console.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 contract Exchange {
+    event DepositToken(
+        address token,
+        address user,
+        uint256 amount,
+        uint256 balance
+    );
 
     address public feeAccount;
+    uint256 public feePercent;
+    // token, msg.sender, balances
+    mapping(address => mapping(address => uint256)) public tokens;
 
-    constructor(address _feeAccount){
+    constructor(address _feeAccount, uint256 _feePercent) {
         feeAccount = _feeAccount;
+        feePercent = _feePercent;
     }
 
+    function depositToken(address _tokenAddress, uint256 _amount) public {
+        IERC20(_tokenAddress).transferFrom(msg.sender, address(this), _amount);
+        tokens[_tokenAddress][msg.sender] += _amount;
+        emit DepositToken(
+            _tokenAddress,
+            msg.sender,
+            _amount,
+            tokens[_tokenAddress][msg.sender]
+        );
+    }
+
+    function getBalanceOf(address _token, address _user)
+        public
+        view
+        returns (uint256)
+    {
+        return tokens[_token][_user];
+    }
 }
--- dex-contract/test/Exchange.js
+++ dex-contract/test/Exchange.js
@@ -6,24 +6,34 @@ const { anyValue } = require("@nomicfoundation/hardhat-chai-matchers/withArgs");
 const { expect } = require("chai");
 const { ethers } = require("hardhat");
 
+
+const FEE_PERCENT = 10;
+
+
 describe("Exchange", function () {
 
   async function deployExchangeFixture() {
     const [deployer, feeAccount, account_1, account_2] = await ethers.getSigners()
     const Exchange = await ethers.getContractFactory("Exchange")
-    const exchange = await Exchange.deploy(feeAccount.address)
+    const exchange = await Exchange.deploy(feeAccount.address, FEE_PERCENT)
     await exchange.deployed()
 
-    return [ exchange, deployer, feeAccount, account_1, account_2 ]
+    return [exchange, deployer, feeAccount, account_1, account_2]
   }
 
   describe("Deployment", function () {
 
-    it("track the fee account", async function(){
+    it("track the fee account", async function () {
 
       const [exchange, deployer, feeAccount, account_1, account_2] = await deployExchangeFixture()
 
       expect(await exchange.feeAccount()).to.equal(feeAccount.address)
+
+    })
+
+    it("fee Percent", async function () {
+      const [exchange, deployer, feeAccount, account_1, account_2] = await deployExchangeFixture()
+      expect(await exchange.feePercent()).to.equal(FEE_PERCENT)
 
     })
 
@@ -33,3 +43,5 @@ describe("Exchange", function () {
 
 
 });
+
+

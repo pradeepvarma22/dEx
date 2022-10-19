63e03bfc2f179348d0991f73fe2ffadc295127f3
pradeepvarma22 <pradeepnadimpalli22@gmail.com>
2022-10-19 17:24:17 +0530

    app: add basic
    Signed-off-by: pradeepvarma22 <pradeepnadimpalli22@gmail.com>

--- dex-app/pages/index.js
+++ dex-app/pages/index.js
@@ -1,12 +1,21 @@
+import { ethers } from 'ethers';
 import Head from 'next/head'
 import Image from 'next/image'
 
 export default function Home() {
-  
+
+
+
+
+
   return (
-    <div className="grid grid-cols-[23%_80%] w-full h-screen text-slate-50	">
-      <div className='bg-slate-800'>A</div>
-      <div className='bg-slate-900'>B</div>
+    <div>
+      <div>
+        <div className="grid grid-cols-[23%_80%] w-full h-screen text-slate-50">
+          <div className='bg-slate-800'>A</div>
+          <div className='bg-slate-900'>B</div>
+        </div>
+      </div >
     </div>
   )
 }
